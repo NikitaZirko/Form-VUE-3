f36bfe270dc65b6a83fc81f0236a57eb49d11453
HOME-PC\Nik <nik.message@gmail.com>
2024-07-16 00:12:51 +0200

    temp

--- assets/HomeView-BMYoocU_.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./BaseLink-BBXIXvad.js";import{_ as a,c as t,w as o,o as r,a as s}from"./index-bfqiZI2d.js";const c={__name:"HomeView",setup(_){return(n,p)=>(r(),t(e,{class:"center",url:"order"},{default:o(()=>[s("Вызвать официанта")]),_:1}))}},d=a(c,[["__scopeId","data-v-9650eec0"]]);export{d as default};
