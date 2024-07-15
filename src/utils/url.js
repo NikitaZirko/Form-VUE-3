223e44d6527b4cf69921b85c8bd9b76ed574e15f
HOME-PC\Nik <nik.message@gmail.com>
2024-07-15 23:46:32 +0200

    deploy

--- src/utils/url.js
+++
@@ -1,5 +0,0 @@
-const url = {
-	BASE_URL: 'https://webhook.site/994c8d78-82a5-4981-bc1a-3f00cfea293f'
-};
-
-export default url;
--- src/views/OrderView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TheFormCall from '@/components/TheFormCall.vue';
-
-</script>
-
-<template lang="pug">
-TheFormCall
-</template>
-
